51276fafac20d7f0cd89f061e1c8051a2daef919
Gaurav Shukla <heygauravshukla@gmail.com>
2025-06-23 10:24:34 +0530

    content: update site title and description
    Replaced default SITE_TITLE and SITE_DESCRIPTION in const.ts with personalized values for Gaurav Shukla's portfolio and blog.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog'
-export const SITE_DESCRIPTION = 'Welcome to my website!'
+export const SITE_TITLE =
+  'Gaurav Shukla - Software engineer, creator, and amateur astronaut.'
+export const SITE_DESCRIPTION =
+  'I’m Gaurav, a software engineer and creator based in India. I do front-end development, build awesome projects and share knowledge through blogs.'
